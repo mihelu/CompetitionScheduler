475dd3b4cf4c07f7f7844ecb132fb5875900ec32
mihelu <misieksala@gmail.com>
2013-01-12 17:47:30 +0100

    webapp project

---
+++ src/main/java/pl/edu/pk/ztbd/competitionscheduler/db/ConnectionResolver.java
@@ -0,0 +1,11 @@
+package main.java.pl.edu.pk.ztbd.competitionscheduler.db;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mhl
+ * Date: 12.01.13
+ * Time: 17:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConnectionResolver {
+}
